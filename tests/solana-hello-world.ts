d1d5f486d50e3ff8faf8672a3c9cb03d2871e272
Miquel A. Cabot <miquel.cabot@gmail.com>
2024-08-02 22:03:03 +0200

    add createWallet() function to tests

--- tests/solana-hello-world.ts
+++ tests/solana-hello-world.ts
@@ -2,6 +2,37 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolanaHelloWorld } from "../target/types/solana_hello_world";
 
+import chai from "chai";
+import { expect } from "chai";
+
+import chaiAsPromised from "chai-as-promised";
+chai.use(chaiAsPromised);
+
+// helper functions
+const createWallet = async (connection: anchor.web3.Connection, funds: number) 
+: Promise<anchor.web3.Keypair> => {
+  const wallet = anchor.web3.Keypair.generate();
+  const tx = await connection.requestAirdrop(
+    wallet.publicKey,
+    anchor.web3.LAMPORTS_PER_SOL * funds
+  );
+  // wait for confirmation
+  const latestBlockHash = await connection.getLatestBlockhash();
+  await connection.confirmTransaction({
+    blockhash: latestBlockHash.blockhash,
+    lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+    signature: tx
+  });
+
+  // check balance
+  const balance = await connection.getBalance(wallet.publicKey);
+  if(balance < funds){
+    throw new Error("Requested amount exceeds target"+
+                    "network's airdrop limit.");
+  }
+  return wallet;
+};
+
 describe("solana-hello-world", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
@@ -10,7 +41,8 @@ describe("solana-hello-world", () => {
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    // const tx = await program.methods.initialize().rpc();
+    console.log("Uep");
+    // console.log("Your transaction signature", tx);
   });
 });
